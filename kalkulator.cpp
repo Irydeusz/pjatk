6a35f07e3f5174df853414b2829b1ad83e927ab1
Michał Cichowski <s20695@pjwstk.edu.pl>
2019-10-24 12:50:05 +0200

    wenezuela

---
+++ kalkulator.cpp
@@ -0,0 +1,33 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+    char op;
+    float num1, num2;
+    cout << "Podaj + - * /";
+    cin >> op;
+    cout << "podaj dwie liczby: ";
+    cin >> num1 >> num2;
+    switch(op)
+    {
+        case '+':
+            cout << num1+num2;
+            break;
+        case '-':
+            cout << num1-num2;
+            break;
+        case '*':
+            cout << num1*num2;
+            break;
+        case '/':
+            cout << num1/num2;
+            break;
+        default:
+
+            cout << "zly operator";
+            break;
+        cout << "wenezuela ma fajna ekonomie nom" << endl;
+        
+    }
+    return 0;
+}
